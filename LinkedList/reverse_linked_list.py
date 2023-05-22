5c44694502c24f8fefdc13338a3d0675d93be5f6
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-05-22 20:09:11 +0800

    Create reverse_linked_list.py

---
+++ LinkedList/reverse_linked_list.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if(head == None):
+            return None
+        else:
+            curr = head
+            prev = None
+            while(curr is not None):
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+            return prev

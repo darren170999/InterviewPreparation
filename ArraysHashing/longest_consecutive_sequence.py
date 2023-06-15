2ea13c9fd19a4a15d72b68b23cad59f9181408d5
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-06-15 17:19:28 +0800

    Create longest_consecutive_sequence.py

---
+++ ArraysHashing/longest_consecutive_sequence.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        ls = [1]
+        max_length = 1
+        n = len(nums)
+        if(n==0):
+            return 0
+        for i in range(0, n-1):
+            j = nums[i]
+            if (nums[i+1] - j == 1):
+                max_length += 1
+            elif(nums[i+1] - j < 0 or nums[i+1] - j > 1):
+                max_length = 1
+            ls.append(max_length)
+        return max(ls)

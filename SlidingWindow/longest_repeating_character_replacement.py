1bac04ae91b8fe122cc27c988a1861736d2db18e
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-06-30 16:50:59 +0800

    Create longest_repeating_character_replacement.py

---
+++ SlidingWindow/longest_repeating_character_replacement.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        # same sliding window approach
+        l = 0
+        n = len(s)
+        count = {}
+        ans = 0
+        for r in range(n):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            while (r-l +1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l+=1
+
+            ans = max(ans, r-l+1)
+        return ans

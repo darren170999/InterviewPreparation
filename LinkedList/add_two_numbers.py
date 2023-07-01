4c3f1d8f423e722a5346a01b99a974ac4579ac53
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-07-01 16:54:14 +0800

    Update add_two_numbers.py

--- LinkedList/add_two_numbers.py
+++ LinkedList/add_two_numbers.py
@@ -1,61 +1,47 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # ones, tens, hundreds
-        head1 = l1 # use head1 as ans
-        head2 = l2
-        carry = 0
-        # Check if same length -> not possible
-        # Alternative, make an ans ll ->. store tabulated 
-        # if ans is None break while
-        added = head1.val + head2.val
-        total = added+ carry
-        if(total>9):
-            carry = 1
-            total = total %10
-        else:
-            carry = 0
-        ans = ListNode(val = total, next=None) # storing answer
-        res = ans
-        head1 = head1.next
-        head2 = head2.next
-        while head1 and head2:# accounted for if they are the same
+        def add_nodes_with_carry(head1, head2, carry):
             added = head1.val + head2.val
             total = added + carry
             if total > 9:
                 carry = 1
-                total = total%10
+                total = total % 10
             else:
                 carry = 0
-            res.next = ListNode(val = total, next=None)
+            return total, carry
+        
+        head1 = l1
+        head2 = l2
+        carry = 0
+        
+        added, carry = add_nodes_with_carry(head1, head2, carry)
+        ans = ListNode(val=added, next=None)
+        res = ans
+        head1 = head1.next
+        head2 = head2.next
+        
+        while head1 and head2:
+            added, carry = add_nodes_with_carry(head1, head2, carry)
+            res.next = ListNode(val=added, next=None)
             res = res.next
             head1 = head1.next
             head2 = head2.next
-        # print(carry)
-        while head1 or head2: # this will append and check for carry diff
-            if head1:
-                total = head1.val + carry
-                if(total>9):
-                    carry = 1
-                    total = total%10
-                else:
-                    carry = 0
-                head1 = head1.next
-            else: # head 2 exists
-                total = head2.val + carry
-                if(total > 9):
-                    carry = 1
-                    total = total%10
-                else:
-                    carry = 0
-                head2 = head2.next                
-            res.next = ListNode(val = total, next=None)
+        
+        remaining = head1 or head2
+        
+        while remaining:
+            total = remaining.val + carry
+            if total > 9:
+                carry = 1
+                total = total % 10
+            else:
+                carry = 0
+            remaining = remaining.next
+            res.next = ListNode(val=total, next=None)
             res = res.next
+        
         if carry == 1:
-            res.next = ListNode(val = carry, next=None)
-            res= res.next
+            res.next = ListNode(val=carry, next=None)
+            res = res.next
+        
         return ans

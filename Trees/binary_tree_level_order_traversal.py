3d9ab05b0794658fd3222b86cef85da5df32094a
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-07-09 17:14:11 +0800

    Update binary_tree_level_order_traversal.py

--- Trees/binary_tree_level_order_traversal.py
+++ Trees/binary_tree_level_order_traversal.py
@@ -1,19 +1,40 @@
+# class Solution:
+#     # must use BFS. Which is implemented with a Queue
+#     # FIFO
+#     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+#         res = []
+#         q = collections.deque() # python lib for queue
+#         q.append(root)
+#         while q:
+#             qLen = len(q)
+#             level = []
+#             for i in range(qLen):
+#                 node = q.popleft()
+#                 if node: # prevent None from appending to level
+#                     level.append(node.val)
+#                     q.append(node.left)
+#                     q.append(node.right)
+#             if level: # level could be empty
+#                 res.append(level)
+#         return res
 class Solution:
-    # must use BFS. Which is implemented with a Queue
-    # FIFO
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        res = []
-        q = collections.deque() # python lib for queue
-        q.append(root)
-        while q:
-            qLen = len(q)
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        ans = []
+        queue = [root]
+
+        while queue:
             level = []
-            for i in range(qLen):
-                node = q.popleft()
-                if node: # prevent None from appending to level
-                    level.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
-            if level: # level could be empty
-                res.append(level)
-        return res
+
+            for node in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            ans.append(level)
+
+        return ans

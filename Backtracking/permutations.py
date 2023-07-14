152cce03fe3d020abc8b601161686227fbae34a4
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-07-14 17:32:47 +0800

    Create permutations.py

---
+++ Backtracking/permutations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        # return clause
+        if len(nums) == 1:
+            return [nums.copy()]
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+            for p in perms: # add original numbers removed
+                p.append(n)
+            ans.extend(perms) # add them to result
+            nums.append(n) # clean up here by adding it back
+        return ans

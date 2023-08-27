007fea9c683a68b06226a146bfc6bcf2532cecbc
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-08-27 18:11:25 +0800

    Create alien_dictionary.py

---
+++ AdvancedGraphs/alien_dictionary.py
@@ -0,0 +1,33 @@
+# Topological Sort, involves DAG
+# a Directed Acyclic Graph a graph with directed edges but no cycles
+# Post Order DFS
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        adj = { c: set() for w in words for c in w} # set so theres not duplicates
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i+1]
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+        visit = {} # False = visited, true= current path
+        res = []
+        def dfs(c): # post order DFS
+            if c in visit:
+                return visit[c]
+            visit[c] = True
+            for nei in adj[c]:
+                if dfs(nei):
+                    return True
+
+            visit[c] = False
+            res.append(c)
+        for c in adj:
+            if dfs(c):
+                return ""
+        res.reverse()
+        return "".join(res)
+
